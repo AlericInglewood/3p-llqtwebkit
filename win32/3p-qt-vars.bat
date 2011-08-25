1ae85eb0a43e2e6e3380bfc70a19b914f710d3f0
callum <callum@callum-iMac.lindenlab.com>
2011-08-25 16:06:06 -0700

    Tweaks to Win32 specific batch/command pronmpts specific to my system

--- win32/3p-qt-vars.bat
+++ win32/3p-qt-vars.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Setting up a Qt environment using 3p-qt HG repository
-set QTDIR=E:\Work\3p-qt\stage
-set PATH=E:\Work\3p-qt\stage\bin;%PATH%
+set QTDIR=C:\Work\3p-llqtwebkit\stage
+set PATH=C:\Work\3p-llqtwebkit\stage\bin;%PATH%
 set QMAKESPEC=win32-msvc2010
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
